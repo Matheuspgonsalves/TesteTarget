3b297945cae3f94a64faaa299e75278a9ac7fd25
Matheus Pereira Gonsalves <matheus.gonsalvespereira@gmail.com>
2024-03-06 21:14:32 -0300

    first commit

---
+++ Q2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Q2{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num;
+
+        //instrucao para usuario
+        System.out.println("Informe o numero: ");
+        num = sc.nextInt();
+
+        isSequencia(num);
+
+        sc.close();
+    }
+
+    public static int isSequencia(int num){
+        int primeiroTermo = 0;
+        int segundoTermo = 1;
+        
+        for(int i = 0; i <= num; i++){
+            
+        }
+        return num;
+    }
+
+    
+}
